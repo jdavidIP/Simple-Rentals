7a57dcf508b30139d9c853acdd65283ab9b6fe6a
Artem Maksymov <42908929+armaksymov@users.noreply.github.com>
2025-02-10 14:06:16 -0500

    Added Profile Picture Display to the main profile page

--- simpleRentals/simpleRentals/urls.py
+++ simpleRentals/simpleRentals/urls.py
@@ -18,6 +18,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from marketplace import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -27,3 +29,5 @@ urlpatterns = [
     path("login/", views.login_view, name="login"),
     path("edit-profile/", views.edit_profile, name="edit_profile"),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
