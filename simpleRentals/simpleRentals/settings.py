bf0447497f08e25f4c3a4dc95a4c22360f46dfbc
MateoVG-coding <mateovega10@hotmail.com>
2025-08-01 01:31:10 -0400

    Add email provider for production.

--- simpleRentals/simpleRentals/settings.py
+++ simpleRentals/simpleRentals/settings.py
@@ -16,6 +16,9 @@ from corsheaders.defaults import default_headers
 from dotenv import load_dotenv
 import os
 
+# Load environment variables from .env file
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -178,8 +181,16 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 ]
 
 # EMAIL
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"   # For development
-# DEFAULT_FROM_EMAIL = "noreply@simplerentals.com" <-- Uncomment and set email here for production
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"   # For development
+#For production, use SMTP settings: 
+EMAIL_BACKEND = os.getenv("EMAIL_BACKEND", "django.core.mail.backends.console.EmailBackend")
+EMAIL_HOST = os.getenv("EMAIL_HOST", "")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", EMAIL_HOST_USER)
+
 
 # URL for frontend in verification links
 FRONTEND_URL = "http://localhost:5173"
