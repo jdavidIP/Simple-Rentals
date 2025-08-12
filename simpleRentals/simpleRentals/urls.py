26b768a58503ede65a1aafe5f52a0af1c0fa4d15
Jose Ibanez <ibapol2003@gmail.com>
2025-08-12 01:59:48 -0400

    Update urls.py

--- simpleRentals/simpleRentals/urls.py
+++ simpleRentals/simpleRentals/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from marketplace import views
 from django.conf.urls.static import static
 from django.conf import settings
@@ -68,6 +68,5 @@ urlpatterns = [
     path("password-reset/confirm/", views.PasswordResetConfirmView.as_view(), name="password_reset_confirm")
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
